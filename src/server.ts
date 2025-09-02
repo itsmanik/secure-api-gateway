f06aabf31fc5793bd0b9550358f73da3e750cf15
itsmanik <manik.hilalpure@gmail.com>
2025-09-02 14:32:35 +0530

    fix: add build and small fixes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { app } from './app.js';
 
-const port = Number(process.env.port ?? 4000);
+const port = Number(process.env.PORT ?? 4000);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
